e8863b3c5fd18119f8cb67d31c8bce44b1267546
shpokas <s.shpokas@gmail.com>
2024-12-05 19:21:50 +0200

    feat: add Footer component with simple design

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 E-shop. All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
