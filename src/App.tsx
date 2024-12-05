3795cb847f2c1152d145005aacd066534928da92
shpokas <s.shpokas@gmail.com>
2024-12-05 18:58:53 +0200

    feat: add global styles for consistent typography and color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <h1 className="text-bold text-4xl text-red-800"></h1>
+      <h1 className="text-bold text-4xl text-red-800">test</h1>
     </div>
   );
 }
