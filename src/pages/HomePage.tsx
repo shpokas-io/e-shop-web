82efe26522b18b350fea99dce1ea01899c10434f
shpokas <s.shpokas@gmail.com>
2024-12-05 19:23:43 +0200

    feat: add HomePage with Header, HeroSection, and Footer

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header/Header";
+import HeroSection from "../components/Hero/HeroSection";
+import Footer from "../components/Footer/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
