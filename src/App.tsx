58df65e01d446bf6e8ee0cd5515109fdbcb2f736
shpokas <s.shpokas@gmail.com>
2024-12-12 16:23:20 +0200

    feat: Add routes for HomePage and AuthenticationPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import AuthenticationPage from "./Pages/AuthenticationPage";
 
 const App: React.FC = () => {
-  return <HomePage />;
+  return (
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthenticationPage />} />
+      </Routes>
+  );
 };
 
 export default App;
