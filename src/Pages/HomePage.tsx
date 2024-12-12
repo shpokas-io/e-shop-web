b05f4003efec6027ce4c1bdc1f844b69303f5470
shpokas <s.shpokas@gmail.com>
2024-12-12 11:48:36 +0200

    refactor: Simplify layout logic and improve spacing consistency in HomePage.tsx

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "../components/Header/Header";
 import DesktopNav from "../components/DesktopNav/DesktopNav";
 import { useTheme } from "@mui/material/styles";
@@ -10,10 +9,9 @@ import HeroSection from "../components/HeroSection/HeroSection";
 import CategorySection from "../components/CategorySection/CategorySection";
 import Box from "@mui/material/Box";
 
-const HomePage: React.FC = () => {
+const HomePage = () => {
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
-  const isTabletOrMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
     <Box
@@ -24,19 +22,16 @@ const HomePage: React.FC = () => {
       }}
     >
       <Header />
-      {isDesktop && (
+      {isDesktop ? (
         <>
           <DesktopNav />
           <HeroSection />
           <CategorySection />
         </>
+      ) : (
+        <CategorySectionMobile />
       )}
-      {isTabletOrMobile && (
-        <>
-          <CategorySectionMobile />
-        </>
-      )}
-      <Box sx={{ flex: 1, paddingBottom: "40px" }}>
+      <Box sx={{ flex: 1, pb: 5 }}>
         <ProductCarousel />
       </Box>
       <Footer />
